e70212c3b0f4675a46d609a03e3239fb00469477
mhinton <hintonmichael80@yahoo.com>
2022-02-11 23:33:07 -0500

    Image from URL disk caching

--- Item Shop Demo/Shop/SwiftUI+REST/Networking/Services/ItemPictureService.swift
+++ Item Shop Demo/Shop/SwiftUI+REST/Networking/Services/ItemPictureService.swift
@@ -32,8 +32,28 @@ final class ItemPictureService: ItemPictureServiceProtocol {
             return Fail<Data, Error>(error: NetworkError.invalidUrl).eraseToAnyPublisher()
         }
         
+        // Attempt to retrieve image from disk
+        // (this could also be done using NSCache, but I'd prefer saving S3 bandwidth on future image requests)
+        let imageName = url.lastPathComponent
+        let cacheLocation = FileManager.default.urls(for: .documentDirectory, in: .userDomainMask)[0].appendingPathComponent(imageName)
+        if FileManager().fileExists(atPath: cacheLocation.path) {
+            do {
+                let data = try Data(contentsOf: cacheLocation)
+                return CurrentValueSubject(data).eraseToAnyPublisher()
+            } catch {
+                print("thought file from \(url) was available locally, but reading failed: ", error)
+            }
+        }
+        
         return networker.getData(url: url, headers: [:])
             .mapError { _ in ItemPictureError.dataInvalid }
+            .handleEvents(receiveOutput: { data in
+                do {
+                    try data.write(to: cacheLocation)
+                } catch {
+                    print("error saving file from \(url): ", error)
+                }
+            })
             .eraseToAnyPublisher()
     }
 }
